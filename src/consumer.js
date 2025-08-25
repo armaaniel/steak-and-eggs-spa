a5813ab42997ebafa3778752220d03eb29365265
armaaniel <ishmaan@hotmail.com>
2025-08-24 18:19:49 -0600

    test

--- src/consumer.js
+++ src/consumer.js
@@ -1,6 +1,6 @@
 import { createConsumer } from "@rails/actioncable"
 
-const API = import.meta.env.VITE_API || 'http://localhost:3000'
+const API = (import.meta.env.VITE_API || 'http://localhost:3000').replace(/\/$/, '')
 const WS = API.replace('https://', 'wss://').replace('http://', 'ws://')
 
 let globalConsumer = null
