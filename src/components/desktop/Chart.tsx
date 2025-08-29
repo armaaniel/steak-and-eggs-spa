62930cfc68e19bd028ff21b169eafd9e883aa4ca
armaaniel <ishmaan@hotmail.com>
2025-08-29 13:17:34 -0600

    types incoming but - for now

--- src/components/desktop/Chart.tsx
+++ src/components/desktop/Chart.tsx
@@ -16,7 +16,7 @@ const Chart = React.memo(({chartData, dataKey}: ChartProps) => {
 				<Line type="monotone" dataKey={dataKey} stroke="#8884d8" strokeWidth={2} dot={false} />
       		<Tooltip cursor={false} position={{ x: 0, y: 0 }} labelFormatter={(index) => chartData[index].date}
 			contentStyle={{ border: 'none', background: 'none', display: 'flex', padding:'4px', gap:'8px' }} 
-			formatter={(value) => [value.toFixed(2), dataKey]}/>
+			formatter={(value) => [(value as number).toFixed(2), dataKey]}/>
           <YAxis domain={[dataMin => (dataMin*0.95), dataMax => (dataMax * 1.05)]} hide={true} />
 			
     		</LineChart>		
