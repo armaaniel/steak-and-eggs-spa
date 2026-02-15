3d0b9601dea166a71587c175e3b4a7a257d31b47
armaaniel <ishmaan@hotmail.com>
2026-02-14 18:13:40 -0700

    use normal domain for gql endpoint to resolve cert error

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'https://steakneggs-prod.eba-fjpi7jhr.us-west-1.elasticbeanstalk.com/graphql',
+  uri: 'https://steakneggs.app/graphql',
 });
 
 const client = new ApolloClient({
