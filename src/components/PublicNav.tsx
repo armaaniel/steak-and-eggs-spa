caccdafab18be2111d365d985f777b67c3e744e0
armaaniel <ishmaan@hotmail.com>
2026-08-23 11:21:47 -0600

    extract PublicNav from Public layout

---
+++ src/components/PublicNav.tsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom'
+import Logo from './Logo'
+
+const PublicNav = () => {
+  const token = localStorage.getItem('authToken')
+
+  return (
+    <nav className="nav">
+      <div className="nav-left">
+        <Link to="/">
+          <Logo />
+        </Link>
+
+        <Link to="/datacat" className="nav-text" state={{ authBoolean: false }}>
+          <span>DataCat (APM)</span>
+        </Link>
+
+        <a href="https://github.com/armaaniel" className="nav-text" target="_blank" rel="noopener noreferrer">
+          <span>Github</span>
+        </a>
+      </div>
+
+      <div className="nav-right">
+        <a href="https://www.notion.so/Steak-Eggs-3487e61da1f98087811cd2dd38b7f662?source=copy_link" className="nav-text" target="_blank" rel="noopener noreferrer">
+          <span>Architecture</span>
+        </a>
+
+        <Link to={token ? '/home' : '/login'} className="btn btn-secondary">
+          Log In
+        </Link>
+
+        <Link to={token ? '/home' : '/signup'} className="btn btn-primary">
+          Sign Up
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default PublicNav
--- src/layouts/Public.tsx
+++ src/layouts/Public.tsx
@@ -1,44 +1,11 @@
 import { Outlet } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import Logo from '../components/Logo'
+import PublicNav from '../components/PublicNav'
 import '../stylesheets/public.css'
 
 function Public() {
-  const token = localStorage.getItem('authToken')
-
   return (
     <>
-      <header>
-        <nav className="nav">
-          <div className="nav-left">
-            <Link to="/">
-              <Logo />
-            </Link>
-
-            <Link to="/datacat" className="nav-text" state={{ authBoolean: false }}>
-              <span>DataCat (APM)</span>
-            </Link>
-
-            <a href="https://github.com/armaaniel" className="nav-text" target="_blank" rel="noopener noreferrer">
-              <span>Github</span>
-            </a>
-          </div>
-
-          <div className="nav-right">
-            <a href="https://www.notion.so/Steak-Eggs-3487e61da1f98087811cd2dd38b7f662?source=copy_link" className="nav-text" target="_blank" rel="noopener noreferrer">
-              <span>Architecture</span>
-            </a>
-
-            <Link to={token ? '/home' : '/login'} className="btn btn-secondary">
-              Log In
-            </Link>
-
-            <Link to={token ? '/home' : '/signup'} className="btn btn-primary">
-              Sign Up
-            </Link>
-          </div>
-        </nav>
-      </header>
+      <header><PublicNav /></header>
 
       <Outlet />
     </>
